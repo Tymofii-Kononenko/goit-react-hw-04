c25e9e80d20237bf495c2a677e2115c2d9780e88
Tymofii Kononenko <timajw95@gmail.com>
2025-01-10 16:51:40 +0100

    01

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import "../components/App.css";
+import Profile from "./Profile/Profile";
+import userData from "../userData.json";
+import friends from "../friends.json";
+import FriendList from "../components/FriendList/FriendList";
+
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+    </>
+  );
+};
+
+export default App;
